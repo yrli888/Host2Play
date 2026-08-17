eeaf5452e1fc3fa84607ddfd2abdb477013bab31
yrli888 <175896491+yrli888@users.noreply.github.com>
2026-08-17 14:01:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=65bd476a-deb6-4585-87fb-7adbe34809e8",
+    "https://host2play.gratis/server/renew?i=44f3c2b5-4b6c-4406-9c7d-9d68bb94bffc",
     # 添加更多链接
 ]
 
